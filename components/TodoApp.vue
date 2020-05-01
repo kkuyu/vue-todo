d6827e5e00cb2fccad42302dcb3af87afcc18ef5
kkuyu <devils67@naver.com>
2020-05-02 00:58:12 +0900

    TodoItem 컴포넌트만들기 - 출력 모드

--- components/TodoApp.vue
+++ components/TodoApp.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+    <todo-item
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @update-todo="updateTodo"
+      @delete-todo="deleteTodo"
+    />
+    <hr />
     <todo-creator @create-todo="createTodo" />
   </div>
 </template>
@@ -59,6 +66,12 @@ export default {
         .get("todos")
         .push(newTodo)
         .write();
+    },
+    updateTodo() {
+      console.log("updateTodo");
+    },
+    deleteTodo() {
+      console.log("deleteTodo");
     }
   }
 };
--- components/TodoItem.vue
+++ components/TodoItem.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>Todo Item</h1>
+  <div class="todo-item">
+    <input type="checkbox" v-model="done" />
+    <div class="item__title-wrap">
+      <div class="item__title">{{todo.title}}</div>
+      <div class="item__date">{{date}}</div>
+    </div>
+    <div class="item__actions">
+      <button type="button" @click="onEditMode">수정</button>
+      <button type="button" @click="deleteTodo">삭제</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import dayjs from "dayjs";
+
+export default {
+  props: {
+    todo: Object
+  },
+  computed: {
+    done: {
+      get() {
+        return this.todo.done;
+      },
+      set(currentValueDone) {
+        this.updateTodo({
+          done: currentValueDone
+        });
+      }
+    },
+    date() {
+      const date = dayjs(this.todo.createAt);
+      const isSame = date.isSame(this.todo.updateAt);
+
+      if (isSame) {
+        return date.format("YYYY년 MM월 DD일");
+      } else {
+        return `${date.format("YYYY년 MM월 DD일")} (edited)`;
+      }
+    }
+  },
+  methods: {
+    onEditMode() {},
+    offEditMode() {},
+    updateTodo(value) {
+      this.$emit("update-todo", this.todo, value);
+    },
+    deleteTodo() {
+      this.$emit("delete-todo", this.todo);
+    }
+  }
+};
+</script>

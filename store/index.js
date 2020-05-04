f6f681bf05ac579a34ec84d4f8dd6073767da894
kkuyu <devils67@naver.com>
2020-05-04 23:09:10 +0900

    Vuex 설치

---
+++ store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue"
+import Vuex from "vuex"
+
+import todoApp from "./todoApp"
+
+Vue.use(Vuex);
+
+export default new Vue.Store({
+  strict: process.env.node_env !== 'production',
+  // // Data
+  // state: {},
+  // // Computed
+  // getters: {},
+  // // Methods: 실제 값을 변경할 때(비동기X, state 변경O)
+  // mutations: {},
+  // // Methods: 일반 로직(비동기O, state 변경X)
+  // actions: {}
+  modules: {
+    todoApp
+  }
+})
